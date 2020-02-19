731decd636d6fb8a947e4e1965fef4fcb06d518c
grupo-02 <vinicius_rx@hotmail.com>
2020-02-19 20:36:55 -0300

    Classe correta enviada

---
+++ Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Retangulo
+{
+    private double baseRetangulo;
+    private double alturaRetangulo;
+
+    public Retangulo(double baseRetangulo, double alturaRetangulo)
+    {
+        this.alturaRetangulo = alturaRetangulo;
+        this.baseRetangulo = baseRetangulo;
+    }
+
+    public double calculaArea()
+    {
+        return this.baseRetangulo * this.alturaRetangulo;
+    }
+
+    public double calculaPerimetro()
+    {
+        return (2 * alturaRetangulo) + (2 * baseRetangulo);
+    }
+
+    public override string toString()
+    {
+        return calculaArea() + calculaPerimetro();
+    }
+}
