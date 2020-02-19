a7d1bc8a4a6716db9153accb8e9ad7ecaad5de7d
lucasbalbi <lucasmvbalbi20@gmail.com>
2020-02-19 19:32:05 -0300

    Calc triangulo

---
+++ Triangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Triangulo
+    {
+
+        public double perimetroTriangulo(double ax, double bx, double cx)
+        {
+
+            double px = (ax + bx + cx) / 2;
+            return px;
+        }
+
+        public double areaTriangulo(double ax, double bx, double cx, double px)
+        {
+
+            double atx = Math.Sqrt(px * (px - ax) * (px - bx) * (px - cx));
+            return atx;
+
+        }
+
+        
+
+
+    }
+}
