975a8b2cb522044b0ec9048563cad06dcd59945e
grupo-02 <vinicius_rx@hotmail.com>
2020-02-19 19:33:15 -0300

    Exercio grupo 02

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRetangulo, alturaRetangulo, areaRetangulo;
+
+            Console.WriteLine("Informe a base e altura de um retangulo:");
+            baseRetangulo = double.Parse(Console.ReadLine());
+            alturaRetangulo = double.Parse(Console.ReadLine());
+
+            areaRetangulo = baseRetangulo * alturaRetangulo;
+
+            Console.WriteLine("A area do retangulo informado é " + areaRetangulo);
+
+            Console.ReadLine();
+        }
+    }
+}
